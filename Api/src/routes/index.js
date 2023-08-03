7ee85714f3afd581193261ca66b3485df6cc0670
maiariaboypujadas <maiariaboy24@gmail.com>
2023-08-03 20:38:50 -0300

    post ruta

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,8 +1,17 @@
 
 const { Router } = require("express");
 const router = Router();
-
+const Job = require ("../models/Job")
 router.get("/empleador");
+router.post("/create-service", async (req, res) => {
+    const {name, address, description, ability, phone, image} = req.body;
+    try {
+const service = await postJobs(name, address, description, ability, phone, image);
+res.json({error: null, service})
+    } catch (error) {
+res.status(400).json({error: error.message})
+    }
+})
 
 module.exports = router;
 
