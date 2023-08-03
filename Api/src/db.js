adbd720a1c9674eebf6a4595a3751e84e2a2bb01
raul2216 <raulpcq22@gmail.com>
2023-08-03 15:49:38 -0500

    cambio en db

--- Api/src/db.js
+++ Api/src/db.js
@@ -25,9 +25,9 @@ async function run() {
         await client.close();
     }
 }
+const User = require("./models/User")
 const Payment = require("./models/Payment")
 const Job = require("./models/Job")
-const User = require("./models/User")
 
 
 run().catch(console.dir);
