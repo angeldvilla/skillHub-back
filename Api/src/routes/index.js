c46a70757c008dcbc7d4e33bb86d8f96fc0dbf56
Magali18 <pereyrahmagali@gmail.com>
2023-08-03 22:16:10 -0300

    ok

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,9 +1,18 @@
 
 const { Router } = require("express");
 const router = Router();
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+
+>>>>>>> Stashed changes
 const Job = require ("../models/Job")
+=======
+const postJobs = require ("../controllers/postServices")
+>>>>>>> fcecba949b9d6abbb6157ed772887ddce65993a1
 router.get("/empleador");
-router.post("/create-service", async (req, res) => {
+router.post("/create", async (req, res) => {
     const {name, address, description, ability, phone, image} = req.body;
     try {
 const service = await postJobs(name, address, description, ability, phone, image);
