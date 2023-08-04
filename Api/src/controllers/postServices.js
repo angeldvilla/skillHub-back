99a27988f5fe3afe53d1326ca228d599cfa77804
maiariaboypujadas <maiariaboy24@gmail.com>
2023-08-04 10:48:31 -0300

    modifique postservice y el handler

--- Api/src/controllers/postServices.js
+++ Api/src/controllers/postServices.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Job = require('../models/Job');
 
-const postJobs = async ( title, users, image,phone, ability, description, address ) => {
+const createPost = async ( title, users, image,phone, ability, description, address ) => {
   try {
     const job = new Job({  title, users, image,phone, ability, description, address  });
      job.save();
@@ -10,14 +10,5 @@ const postJobs = async ( title, users, image,phone, ability, description, addres
     throw new Error('Could not create service');
   }
 };
-module.exports = postJobs;
+module.exports = createPost;
 
-// router.post("/:userId/create-service", async (req, res) => {
-//     const {name, address, description, ability, phone, image} = req.body;
-//     try {
-// const service = await postJobs(name, address, description, ability, phone, image);
-// res.json({error: null, service})
-//     } catch (error) {
-// res.status(400).json({error: error.message})
-//     }
-// })
