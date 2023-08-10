00dfc0a018b1c8942cdfc0de829f2ee72c05ee69
NoistNT <arielgnr23@gmail.com>
2023-08-10 17:24:03 -0300

    update getUserById error handling

--- Api/src/controllers/getUser.js
+++ Api/src/controllers/getUser.js
@@ -3,9 +3,12 @@ const User = require("../models/User");
 const getUserById = async (id) => {
   try {
     const user = await User.findOne({ uid: id });
+    if (!user) {
+      throw new Error("User not found");
+    }
     return user;
   } catch (error) {
-    throw new Error("User not found");
+    console.log(error.message);
   }
 };
 
