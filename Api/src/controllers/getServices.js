a9da6c4f45cb06893da3f2c2b77890741f650df1
maiariaboypujadas <maiariaboy24@gmail.com>
2023-08-12 14:49:54 -0300

    modificado getService

--- Api/src/controllers/getServices.js
+++ Api/src/controllers/getServices.js
@@ -1,23 +1,22 @@
-const mongoose = require ('mongoose');
-const Job = require ('../models/Job');
+const mongoose = require('mongoose');
+const Job = require('../models/Job');
 
-const getAllJobs = async (_id, title) => {
-    let jobs;
-    try {
-        if(_id) {
-             jobs = await Job.find({_id})
-        } else if (title) {
-            
-            jobs = await Job.find({title})
-
-        } else {
-            jobs = await Job.find()
-        }
-        return jobs;
-    } catch (error) {
-        throw new Error('DataBase not found')
+const getAllJobs = async (id, title) => {
+  let jobs;
+  try {
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      jobs = await Job.find({ _id: id });
+    } else {
+      jobs = await Job.find({ uid: id });
     }
-}
+    if (jobs.length === 0 && title) {
+      jobs = await Job.find({ title });
+    }
+    return jobs;
+  } catch (error) {
+    throw new Error('Database error');
+  }
+};
 
 module.exports = getAllJobs;
 
