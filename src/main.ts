453c2c0f88dd724a0b733da92f2df9ac27bfc214
NoistNT <arielgnr23@gmail.com>
2024-06-13 18:06:45 -0300

    Fix env variable not working in main app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
-const { PORT = 3001 } = process.env;
+config({ path: './.env' });
+
+const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  if (!PORT) throw new Error('PORT is not defined');
+
   await app.listen(PORT);
 }
-
 bootstrap();
