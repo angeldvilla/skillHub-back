c293838b742d7963d2e29a3543351627bc33a147
NoistNT <arielgnr23@gmail.com>
2024-06-13 11:34:56 -0300

    Enable cors and add port to main app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { PORT = 3001 } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(PORT);
 }
+
 bootstrap();
