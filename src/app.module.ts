a973034bbd99590392dcefb3694371c8958dc583
NoistNT <arielgnr23@gmail.com>
2024-06-13 18:06:16 -0300

    Add drizzle connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import * as schema from '@/db/schema';
+import { DrizzlePGModule } from '@knaadh/nestjs-drizzle-pg';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JobModule } from './job/job.module';
+import { SeederModule } from './seed/seed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DrizzlePGModule.register({
+      tag: 'DB_DEV',
+      pg: {
+        config: {
+          connectionString: process.env.DATABASE_URL,
+        },
+        connection: 'pool',
+      },
+      config: { schema: { ...schema } },
+    }),
+    JobModule,
+    SeederModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
