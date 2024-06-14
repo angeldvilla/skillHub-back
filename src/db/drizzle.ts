5febf75da56dc44f462f2ed8efc74d3e7dfb85f3
NoistNT <arielgnr23@gmail.com>
2024-06-14 15:19:19 -0300

    Add drizzle config path to env variables

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,9 @@
 import { neon } from '@neondatabase/serverless';
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
+
+config({ path: './.env' });
 
 const { DATABASE_URL } = process.env;
 
