7f70c2c050c7377aaeb004f6f19676c0c7baabce
NoistNT <arielgnr23@gmail.com>
2024-06-27 12:35:23 -0300

    Refactor main app

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
+import { PORT } from '@/config';
 import { NestFactory } from '@nestjs/core';
-import { config } from 'dotenv';
 import { AppModule } from './app.module';
-
-config({ path: './.env' });
-
-const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  if (!PORT) throw new Error('PORT is not defined');
 
   await app.listen(PORT);
 }
