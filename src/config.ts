8fa51706e1b15e7f8c2eae69c3acfc79a841726f
NoistNT <arielgnr23@gmail.com>
2024-06-27 12:35:03 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,20 @@ import { config } from 'dotenv';
 
 config();
 
-const msg = 'Missing environment variable:';
+const msg = 'Missing environment variable';
+
+if (!process.env.PORT) {
+  throw new Error(`${msg}: PORT`);
+}
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(`${msg} DATABASE_URL`);
+  throw new Error(`${msg}: DATABASE_URL`);
 }
 
 if (!process.env.JWT_SECRET) {
-  throw new Error(`${msg} JWT_SECRET`);
+  throw new Error(`${msg}: JWT_SECRET`);
 }
 
+export const PORT = process.env.PORT;
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
