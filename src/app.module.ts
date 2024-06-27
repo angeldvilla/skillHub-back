26e89fc76af1494a54eae0bcddbc414cac6b4447
NoistNT <arielgnr23@gmail.com>
2024-06-27 00:36:18 -0300

    Add auth module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 import { JobModule } from './job/job.module';
 import { SeederModule } from './seed/seed.module';
 import { UserModule } from './user/user.module';
-import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { CategoryModule } from './category/category.module';
     SeederModule,
     UserModule,
     CategoryModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
