fd3905f81f9221fae02b4a829a6d15b0ce377a7a
NoistNT <arielgnr23@gmail.com>
2024-06-27 12:12:56 -0300

    Add jwt guard and strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { JWT_SECRET } from '@/config';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    // We could do a DB lookup here to extract more info about the user
+    return { userId: payload.sub, username: payload.username };
+  }
+}
